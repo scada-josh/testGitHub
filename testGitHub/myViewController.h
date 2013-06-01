a0da0cecef5327035266e47bd4279e60dbb31bd8
scada-josh <scada.josh@gmail.com>
2013-06-01 23:44:03 +0700

    add myViewController

---
+++ testGitHub/myViewController.h
@@ -0,0 +1,13 @@
+//
+//  myViewController.h
+//  testGitHub
+//
+//  Created by THE JOSH on 6/1/56 BE.
+//  Copyright (c) 2556 THE JOSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myViewController : UIViewController
+
+@end
---
+++ testGitHub/myViewController.m
@@ -0,0 +1,38 @@
+//
+//  myViewController.m
+//  testGitHub
+//
+//  Created by THE JOSH on 6/1/56 BE.
+//  Copyright (c) 2556 THE JOSH. All rights reserved.
+//
+
+#import "myViewController.h"
+
+@interface myViewController ()
+
+@end
+
+@implementation myViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
